5f232f18e9fdec17b80fb0bcc533c6c7a163c4a9
vvvvo0 <idvvvvo@gmail.com>
2024-12-09 00:52:31 +0900

    switch 문

--- MainApp.cs
+++ MainApp.cs
@@ -10,28 +10,31 @@ namespace Hello //비슷한 것들끼리 하나의 이름 아래 묶음.
          //static은 한정자. 메소드나 변수 등을 수식.
          // static 키워드로 수식되는 코드는 프로그램이 처음 구동될 때부터 메모리에 할당된다. 
         {
-            Console.Write("요일을 입력하세요(일, 월, 화) :");
+            object obj = null;
 
-            string day = Console.ReadLine(); // c의 scanf와 비슷
+            string s = Console.ReadLine(); //문자열 입력 받음
+            if (int.TryParse(s, out int out_i)) //Tryparse는 Parse와 같은 기능(문자열을 숫자로 변환).
+                                                //out 키워드는 출력 전용 매개변수임을 나타내는 용도로 쓰임
+                obj = out_i; 
+            else if (int.TryParse(s, out int out_f))
+                obj = out_f;
+            else
+                obj = s;
 
-            switch (day)
+            switch(obj) //박싱된 데이터의 형식에 따라 메시지 출력
             {
-                case "일":
-                    Console.WriteLine("Sundat");
-                    break;
-                case "월":
-                    Console.WriteLine("Monday");
-                    break;
-                case "화":
-                    Console.WriteLine("Tuesday");
+                case int:
+                    Console.WriteLine($"{(int)obj}는 int 형식입니다.");
                     break;
 
-
-
-
+                case float:
+                    Console.WriteLine($"{(float)obj}는 float 형식입니다.");
+                    break;
+                default:
+                    Console.WriteLine($"{(obj)}(은)는 모르는 형식입니다.");
+                    break;
 
             }
-            
         }
     }
 }
