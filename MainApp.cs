0e6701a29a11606140ee0e808b867f67b12af427
vvvvo0 <idvvvvo@gmail.com>
2024-12-09 18:15:45 +0900

    goto 문

--- MainApp.cs
+++ MainApp.cs
@@ -2,22 +2,42 @@
 
 namespace Hello 
 {
-    class MainApp 
+    class MainApp
     {
-        static void Main(string[] args)  
+        static void Main(string[] args)
         {
+            Console.Write("종료 조건(숫자)을 입력하세요． :");
 
-            int[] numbers = new int[] { 0, 1, 2, 3, 4 }; //new int[]를 사용하여 0부터 4까지의 정수 값을 가진 numbers 배열을 선언
+            String input = Console.ReadLine();
 
-            //int[] numbers = { 0, 1, 2, 3, 4 }; // 0부터 4까지의 정수 값을 가진 numbers 배열을 선언.
-            //두 가지 방식 모두 배열을 선언하고 초기화하는 데 사용할 수 있는 유효한 방법
-            //new int[]를 사용하는 방식은 배열의 크기를 명시적으로 지정하지 않아도 되는 장점.
+            int input_number = Convert.ToInt32(input);
 
+            int exit_number = 0;
 
-            foreach (int a in numbers) // numbers 배열의 각 요소를 순회하며, 각 요소를 a 변수에 할당
+            for (int i = 0; i < 2; i++)
             {
-                Console.WriteLine(a); // a 변수에 저장된 값을 콘솔에 출력
+                for (int j = 0; j < 2; j++)
+                {
+                    for(int k = 0; k < 3; k++)
+                    {
+                        if (exit_number++ == input_number)
+                            goto EXIT_FOR;
+
+                        Console.WriteLine(exit_number);
+                    }
+                }
+
             }
+
+            goto EXIT_PORGRAM;
+
+        EXIT_FOR:
+            Console.WriteLine("\nExit nested for...");
+
+        EXIT_PORGRAM:
+            Console.WriteLine("Exit program...");
+
+
         }
     }
 }
