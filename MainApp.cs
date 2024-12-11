d30c57822398e456acac59e7a76af896bc45df63
vvvvo0 <idvvvvo@gmail.com>
2024-12-11 21:59:05 +0900

    가변 개수의 인수

--- MainApp.cs
+++ MainApp.cs
@@ -4,43 +4,33 @@ namespace Hello
 {
     class Mainapp
     {
-        static int Plus(int a, int b)
+        static int Sum(params int[] args) // 가변 개수의 인수는 params 키워드와 배열을 이용해서 선언
         {
-            Console.WriteLine("Calling int Plus(int,int)...");
-            return a + b;
-        }
+            Console.Write("Summing... ");
 
-        static int Plus(int a, int b, int c)
-        {
-            Console.WriteLine("Calling int Plus(int,int,int)...");
-            return a + b + c;
-        }
+            int sum = 0;
 
-        static double Plus(double a, double b)
-        {
-            Console.WriteLine("Calling double Plus(double,double)...");
-            return a + b;
-        }
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (i > 0)
+                    Console.Write(", ");
 
-        static double Plus(int a, double b)
-        {
-            Console.WriteLine("Calling double Plus(int, double)...");
-            return a + b;
+                Console.Write(args[i]);
+
+                sum += args[i];
+            }
+            Console.WriteLine();
+
+            return sum;
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine(Plus(1, 2));
-            Console.WriteLine(Plus(1, 2, 3));
-            Console.WriteLine(Plus(1.0, 2.4));
-            Console.WriteLine(Plus(1, 2.4));
+            int sum = Sum(3, 4, 5, 6, 7, 8, 9, 10);
+
+            Console.WriteLine("Sum : {0}", sum);
         }
     }
 }
 
-// 하나의 메소드 이름에 여러 개의 구현을 올리는 것.
-//오버로딩을 해 놓으면 컴파일러가 메소드 호출 코드에 사용되는 매개변수의 수와 형식을 분석해서
-//어떤 버전이 호출될지를 찾아줌.
-
-//실행할 메소드의 버전을 찾는 작업은 컴파일 타임에 이루어지므로 성능 저하 X.
-//이름에 대한 고민을 줄여줌. 코드를 일관성 있게 유지해줌.
+// 형식은 같으나 인수의 개수만 유연하게 달라지는 경우 적합.
