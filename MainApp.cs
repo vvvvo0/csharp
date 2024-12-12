ee1d21b2c2567d4bec6a31805519d06f4ee5fbed
vvvvo0 <idvvvvo@gmail.com>
2024-12-12 23:14:47 +0900

    정적 필드

--- MainApp.cs
+++ MainApp.cs
@@ -1,42 +1,38 @@
 ﻿using System;
 
-namespace Hello
+class Global
 {
-    class Mainapp
+    public static int Count = 0; // static으로 수식한 필드는 프로그램 전체에 걸쳐 하나만 존재.
+                                 // 따라서 프로그램 전체에 걸쳐 공유해야 한느 변수는 정적 필드 이용이 적절.
+}
+
+class ClassA
+{
+    public ClassA()
     {
-        class Cat // 클래스 선언, 모든 클래스는 복합 데이터 형식임. 그리고 복합 데이터 형식은 참조 형식임.
-        {
-            public string Name; // 필드: Name, Color 처럼 클래스 안에 선언된 변수들
-            public string Color;
-
-            public void Meow() //메소드
-            {
-                Console.WriteLine($"{Name} : 야옹");
-            }
-        }
-
-        class MainApp
-        {
-            static void Main(string[] args)
-            {
-                Cat kitty = new Cat(); // kitty 객체 생성,
-                                       // new키워드: 객체 생성에 사용하는 연산자
-                                       // Cat(): 생성자 메소드
-                                       // 매개변수가 없는 버전의 Cat() 생성자는 컴파일러가 자동으로 생성해준
-                                       // 생성자를 호출할 때처럼 사용
-                kitty.Color = "하얀색";
-                kitty.Name = "키티";
-                kitty.Meow();
-                Console.WriteLine($"{kitty.Name} : {kitty.Color}");
-
-                Cat nero = new Cat();
-                nero.Color = "검은색";
-                nero.Name = "네로";
-                nero.Meow();
-                Console.WriteLine($"{nero.Name} : {nero.Color}");
-            }
-        }
+        Global.Count++; // 다른 클래스인 ClassA에서 Global 클래스의 정적 필드에 접근함.
     }
 }
 
-// 형식은 같으나 인수의 개수만 유연하게 달라지는 경우 적합.
+class ClassB
+{
+    public ClassB()
+    {
+        Global.Count++; // 다른 클래스인 ClassB에서 Global 클래스의 정적 필드에 접근함.
+    }
+}
+
+class MainApp
+{
+    static void Main()
+    {
+        Console.WriteLine($"Global.Count : {Global.Count}");
+
+        new ClassA();
+        new ClassA();
+        new ClassB();
+        new ClassB();
+
+        Console.WriteLine($"Global.Count : {Global.Count}");
+    }
+}
