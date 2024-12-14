39379d1766e2aedde2cd782cbdb7d9ea9d6035d8
vvvvo0 <idvvvvo@gmail.com>
2024-12-14 23:34:27 +0900

    상속 / 파생 클래스의 객체 생성 과정

--- MainApp.cs
+++ MainApp.cs
@@ -1,25 +1,44 @@
 ﻿using System;
 
-namespace AccessModifier
+namespace Inheritance
 {
-    class WaterHeater
+    class Base // 기본 클래스 선언
     {
-        protected int temperature; // 파생클래스에서 접근 가능
-
-        public void SetTemperature(int temperature)
+        protected string Name; // Name 이라는 필드 선언
+        public Base(string Name) // 기본 클래스의 생성자.
+                                 // 이 클래스를 상속받는 클래스를 만들 때, 각 클래스의 생성자에서
+                                 // Name 필드를 초기화해야 함.
         {
-            if (temperature < -5 || temperature > 42)
-            {
-                throw new Exception("Out of temperature range");
-            }
-
-            this.temperature = temperature; // protected로 수식된 필드이므로 외부에서 직접 접근 불가.
-                                            // 이렇게 public 메소드를 통해 접근해야 함.
+            this.Name = Name;
+            Console.WriteLine($"{this.Name}.Base()");
         }
 
-        internal void TurnOnWater()
+        ~Base()
         {
-            Console.WriteLine($"Turn on water : {temperature}");
+            Console.WriteLine($"{this.Name}.~Base()");
+        }
+
+        public void BaseMethod()
+        {
+            Console.WriteLine($"{Name}.BaseMethod()");
+        }
+    }
+
+    class Derived : Base // 파생 클래스
+    {
+        public Derived(string Name) : base(Name) // 파생클래스의 생성자에서 기본 클래스의 생성자 호출.
+        {
+            Console.WriteLine($"{this.Name}.Derived()"); // 추가적인 초기화 작업
+        }
+
+        ~Derived()
+        {
+            Console.WriteLine($"{this.Name}.~Derived()");
+        }
+
+        public void DerivedMethod()
+        {
+            Console.WriteLine($"{Name}.DerivedMethod()");
         }
     }
 
@@ -27,22 +46,15 @@ namespace AccessModifier
     {
         static void Main(string[] args)
         {
-            try
-            {
-                WaterHeater heater = new WaterHeater();
-                heater.SetTemperature(20);
-                heater.TurnOnWater();
+            Base a = new Base("a"); // a라는 객체 생성. 
+                                    // Base("a") -> 생성자를 호출하여 Name 필드를 "a"로 초기화 함.
+                                    // new 키워드를 사용 -> 클래스의 인스턴스를 생성할 때 생성자가 호출됨.
 
-                heater.SetTemperature(-2);
-                heater.TurnOnWater();
+            a.BaseMethod();
 
-                heater.SetTemperature(50);
-                heater.TurnOnWater();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            Derived b = new Derived("b");
+            b.BaseMethod();
+            b.DerivedMethod();
         }
     }
 }
