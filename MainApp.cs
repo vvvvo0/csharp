1fd16f38d3eb23f82d321f0ca6d704108a3706b4
vvvvo0 <idvvvvo@gmail.com>
2024-12-15 22:48:07 +0900

    오버라이딩 봉인

--- MainApp.cs
+++ MainApp.cs
@@ -1,39 +1,36 @@
 ﻿using System;
 
-namespace MethodHiding
-    // 메소드 숨기기: CLR에게 기반 클래스에서 구현된 버전의 메소드를 감추고,
-    // 파생 클래스에서 구현된 버전만 보여주는 것.
-    // 왜냐하면 어떤 메소드가 향후 오버라이딩될지 안 될지를 판단하는 것이 어렵기 때문에 사용함.
-    // 기반 클래스에서는 아무 생각없이 메소들르 구현해도 메소드 숨기기를 하면 오버라이딩과 같은 효과 얻을 수 있음.
+/* 오버라이딩 봉인: 파생 클래스의 작성자를 위한 기반 클래스 작성자의 배려.
+ 파생 클래스의 작성자가 기반 클래스로부터 상속받은 메소드 하나를 오버라이딩 한 뒤,
+ 이 때문에 오작동하게 된다면, 파생 클래스 작성자는 그 오작동의 원인을 알 길이 없음.
+ 오버라이딩을잘못 해서 문제가 발생할 수 있다면, 봉인 메소드를 이용해서 상속을 사전에 막는 것이 나음.
+ 컴파일할 때 에러가 나오기 때문. */
+
+class Base
 {
-    class Base
+    public virtual void SealMe() // virtual로 선언된 가상 메소드
     {
-        public void MyMethod()
-        {
-            Console.WriteLine("Base.MyMethod()");
-        }
     }
+}
 
-    class Derived : Base
+class Derived : Base
+{
+    public sealed override void SealMe() // virtual로 선언된 가상 메소드를 오버라이딩한 메소드에서만
+                                         // 이렇게 오버라이딩되지 않도록 sealed 키워드를 이용해서 봉인 가능.
     {
-        public new void MyMethod() // 파생클래스의 메소드를 new 한정자로 수식함으로써 메소드 숨기기 가능.
-        {
-            Console.WriteLine("Derived.MyMethod()");
-        }
     }
+}
 
-    class MainApp
+class WantToOverride : Derived
+{
+    public override void SealMe() // 'WantToOverride.SealMe()': 상속된 'Derived.SealMe()' 멤버는 봉인되어 있으므로 재정의할 수 없습니다.
     {
-        static void Main(string[] args)
-        {
-            Base baseObj = new Base();
-            baseObj.MyMethod();
+    }
+}
 
-            Derived derivedObj = new Derived();
-            derivedObj.MyMethod();
-
-            Base baseOrDerived = new Derived(); // 이렇게 객체를 생성하면 CLR에 Base 버전의 MyMethod가 노출되어 이를 실행함.
-            baseOrDerived.MyMethod(); // "Base.MyMethod();" 출력
-        }
+class MainApp
+{
+    static void Main(string[] args)
+    {
     }
 }
