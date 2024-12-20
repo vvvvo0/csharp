b54743b739e1f2b0d3c8ea8f1e9e5a7fde821901
vvvvo0 <idvvvvo@gmail.com>
2024-12-20 20:50:44 +0900

    System.Array 클래스

--- MainApp.cs
+++ MainApp.cs
@@ -2,59 +2,73 @@
 
 
 /*
-인덱스 from end 연산자(^):
-^연산자는 컬렉션의 마지막부터 역순으로 인덱스를 지정하는 기능.
+C#에서는 모든 것이 객체임. 따라서 배열도 객체이므로, 기반이 되는 형식이 있음.
+.NET의 Commin Type System(CTS)에서 배열은 System.Array 클래스에 대응됨.
 
-^1: 배열(컬렉션)의 마지막 요소를 나타내는 인덱스
-^2: 마지막에서 두 번째 요소를 나타내는 인덱스
+즉, C#에서 모든 배열은 System.Array 클래스에서 파생됨.
+
+따라서 모든 배열은 System.Array 클래스의 멤버를 상속받아 사용할 수 있습니다. 
+
+System.Array 클래스는 배열을 위한 기본 클래스로, 배열의 크기, 차원, 정렬, 검색 등 다양한 작업을 수행하는 메서드와 프로퍼티를 제공합니다.
+
+이 System.Array 클래스에 있는 메소드와 프로퍼티들을 사용해서, 
+배열 내부의 데이터를 원하는 순서대로 정렬하거나,특정 데이터를 배열 속에서 찾아내는 작업을 쉽게 할 수 있음.
+예를 들어, Length 프로퍼티를 사용하여 배열의 크기를 가져오거나, Sort() 메서드를 사용하여 배열을 정렬할 수 있음.
  */
 
 
-namespace ArraySample2
+// 배열의 타입과 기본 타입을 출력
+// int 기반의 배열이 System.Array 형식에서 파생됐을을 보여줌
+namespace DerivedFromArray
 {
     class MainApp
     {
         static void Main(string[] args)
         {
-            int[] scores = new int[5]; // 5개의 정수를 저장할 수 있는 scores라는 배열을 선언
+            int[] array = new int[] { 10, 30, 20, 7, 1 }; //  5개의 정수 값을 가진 array 배열을 선언하고 초기화
 
-            scores[0] = 80; // 배열의 첫 번째 요소(scores[0])에 80을 할당
-            scores[1] = 74;
-            scores[2] = 81;
+            Console.WriteLine($"Type Of array : {array.GetType()}"); // array '배열의 타입'을 출력
+                                                                     // array.GetType()은 array 객체의 타입을 나타내는 Type 객체를 반환합니다.
+                                                                     // 1차원 정수형 배열이므로 System.Int32[]가 출력됨. 
 
-            scores[^2] = 90;  // 배열의 마지막-1
-                              // 배열의 마지막에서 두 번째 요소에 90을 할당합니다.
-            scores[^1] = 34;  // 배열의 마지막
-                              //  배열의 마지막 요소에 34를 할당합니다.
-
-
-            foreach (int score in scores) // foreach 문: scores라는 배열의 각 요소를
-                                          // 차례대로 score라는 변수에 담으면서 코드 실행.
-                                          // 각 반복에서 score 변수에는 현재 요소의 값이 할당됨.
-                Console.WriteLine(score); // score 변수의 값을 콘솔에 출력
+            Console.WriteLine($"Base type Of array : {array.GetType().BaseType}");
+            // array '배열의 기본 타입'을 출력
+            // array.GetType().BaseType는 array 객체의 '기본 타입'을 나타내는 Type 객체를 반환합니다.
+            // '모든 배열'은 System.Array 클래스를 상속받으므로 System.Array가 출력됩니다.
 
 
-            int sum = 0; // 점수의 합을 저장할 sum 변수를 선언하고 0으로 초기화
-            foreach (int score in scores) // foreach 문을 사용하여 scores 배열의 각 요소를 순회함
-                sum += score; // 각 반복에서 sum += score;는 sum 변수에 score 변수의 값을 더합니다.
+            // array.GetType()
+            // GetType() 메서드: 모든 객체가 가지고 있는 메서드로, 해당 객체의 타입 정보를 나타내는, Type 객체를 반환합니다.
+            // array는 배열 객체이므로, array.GetType()은 '배열의 타입 정보'를 담고 있는, Type 객체를 반환합니다.
+            // 예를 들어, int[] array = new int[5]; 라면 array.GetType()은 System.Int32[] 타입을 나타내는 Type 객체를 반환합니다.
 
-
-            int average = sum / scores.Length; // sum 변수를 배열의 길이(scores.Length)로 나누어 평균을 계산하고,
-                                               // average 변수에 저장
-
-            Console.WriteLine($"Average Score : {average}"); // 문자열 보간을 사용하여 "Average Score : " 문자열과
-                                                             // average 변수의 값을 결합하여 콘솔에 출력
+            // array.GetType().BaseType
+            // BaseType 프로퍼티는 Type 객체의 프로퍼티(닷넷 문서에서 BaseType이 Type 클래스의 프로퍼티로 정의되어 있기 때문)로,
+            // 해당 타입의 '기본 타입 정보'를 나타내는, Type 객체를 반환합니다.
+            // C#에서 모든 배열은 System.Array 클래스를 상속받습니다.
+            // 따라서 array.GetType().BaseType는 System.Array 타입을 나타내는 Type 객체를 반환합니다.
         }
     }
 }
 
+
 /*
 출력 결과
 
-80
-74
-81
-90
-34
-Average Score : 71
+Type Of array : System.Int32[]
+Base type Of array : System.Array
+ */
+
+
+/*
+(번외)
+배열마다 타입이 다를 수 있습니다.
+C#에서 배열은 int[], string[], double[] 과 같이 저장하는 데이터의 종류에 따라 다른 타입을 가집니다.
+int[] 배열은 정수형 데이터만 저장할 수 있고, string[] 배열은 문자열 데이터만 저장할 수 있으며, 
+double[] 배열은 실수형 데이터만 저장할 수 있습니다.
+
+따라서 GetType() 메서드와 BaseType 프로퍼티를 사용하여 배열의 타입 정보를 확인하고, 
+배열 타입에 따라 다른 작업을 수행하는 코드를 작성할 수 있습니다.
+
+
  */
