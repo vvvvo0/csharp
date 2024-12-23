617d3043a940511a96fc1dd03bc43021f45e960f
vvvvo0 <idvvvvo@gmail.com>
2024-12-23 17:35:46 +0900

    Queue<T>

--- MainApp.cs
+++ MainApp.cs
@@ -1,39 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.Generic; // Queue<T>를 사용하기 위해 필요한 네임스페이스
+
 
 
 /*
-List<T>:
-비일반화 클래스인 ArrayList와 같은 기능을 하지만, 특정 자료형만 저장할 수 있도록 제네릭을 사용한다.
-형식 매개변수에 입력한 형식 외에는 입력을 허용하지 않는다.
-*/
+Queue<T> 제네릭 컬렉션을 사용하는 방법:
+Queue<T>는 선입선출(FIFO) 방식으로 데이터를 저장하는 자료 구조.
+즉, 먼저 들어간 데이터가 먼저 나오는 구조입니다.
+<T>는 큐에 저장될 데이터의 형식을 나타내는 제네릭 타입 매개변수입니다.
+ */
 
 
-namespace UsingGenericList
+// Queue<T> 제네릭 컬렉션을 사용하는 방법을 보여줌
+namespace UsingGenericQueue
 {
     class MainApp
     {
         static void Main(string[] args)
         {
-            List<int> list = new List<int>(); // 정수형(int) 자료형을 저장하는 List 컬렉션 list를 생성합니다. 
-            for (int i = 0; i < 5; i++) //  0부터 4까지의 정수를,
-                list.Add(i); // list에 추가합니다.
+            Queue<int> queue = new Queue<int>(); // int형 데이터를 저장하는 Queue<int> 컬렉션 queue를 생성합니다.
 
-            foreach (int element in list) // list의 모든 요소를 순회하며 각 요소를 element 변수에 할당하고,
-                Console.Write($"{element} "); // 값을 출력
-            Console.WriteLine();
+            queue.Enqueue(1); // queue에 정수 1을 추가합니다. 
+                              // Enqueue() 메서드: 큐의 맨 뒤에 요소를 추가합니다.
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
 
-            list.RemoveAt(2); // list에서 인덱스 2의 요소(값: 2)를 삭제합니다.
-
-            foreach (int element in list)
-                Console.Write($"{element} ");
-            Console.WriteLine();
-
-            list.Insert(2, 2); // list의 인덱스 2에 정수 2를 삽입합니다.
-
-            foreach (int element in list)
-                Console.Write($"{element} ");
-            Console.WriteLine();
+            while (queue.Count > 0) // queue에 요소가 남아있는 동안 반복합니다. 
+                                    // queue.Count: queue에 저장된 요소의 개수를 반환합니다.
+                Console.WriteLine(queue.Dequeue()); // queue에서 맨 앞 요소를 제거하고 반환합니다. 
+                                                    // Dequeue() 메서드: 큐의 맨 앞에서 요소를 제거하고 반환합니다. 
+                                                    // Console.WriteLine() 메서드: 제거된 요소를 콘솔에 출력합니다.
         }
     }
 }
@@ -42,7 +40,9 @@ namespace UsingGenericList
 /*
 출력 결과
 
-0 1 2 3 4 
-0 1 3 4 
-0 1 2 3 4 
+1
+2
+3
+4
+5
 */
