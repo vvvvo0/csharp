aa614bf6fff8dc27cb00c1d6fb65868b6f3592da
vvvvo0 <idvvvvo@gmail.com>
2024-12-24 16:21:41 +0900

    throw 문

--- MainApp.cs
+++ MainApp.cs
@@ -2,56 +2,68 @@
 
 
 /*
-try-catch 문: 
-C#에서 예외를 처리하는 방법.
-IndexOutOfRangeException 예외를 던졌는데 Main() 메서드가 처리하지 못하게 되는 문제 발생.
-이를 해결하려면 예외를 Main()메서드가 받으면(catch) 됨.
+예외 처리(Exception Handling):
+예외 처리는 프로그램 실행 중 발생할 수 있는 예외 상황을 처리하여 
+프로그램의 비정상적인 종료를 방지하고 안정성을 높이는 데 사용됩니다.
+
+throw 문: 예외를 던지는 방법.
+ 예외는
+(1) 발생한 메서드에서 처리할 수도 있고,
+(2) 호출자에게 던져서 호출자가 처리하도록 할 수도 있습니다.
  */
 
-// try-catch 문을 이용하여, 잘못된 인덱스를 통해 배열의 요소에 접근할 때 일어나는
-// IndexOutOfRangeException 예외를 안전하게 받아 처리하는 방법.
-namespace TryCatch
+
+namespace Throw
 {
+
     class MainApp
     {
-        static void Main(string[] args)
+        static void DoSomething(int arg) // DoSomething 메서드: 정수형 인자 arg를 입력으로 받습니다.
+
         {
-            int[] arr = { 1, 2, 3 }; // 크기가 3인 정수형 배열 arr을 선언하고 {1, 2, 3}으로 초기화합니다.
+            if (arg < 10) // arg 값이 10보다 작으면,
+                Console.WriteLine($"arg : {arg}"); // "arg : {arg}"를 출력하고,
 
-            
-            try // 실행하려는 코드
+            else // arg 값이 10보다 크거나 같으면
+                throw new Exception("arg가 10보다 큽니다."); // throw 문을 이용해서
+                                                             // Exception 예외를 발생시킵니다.
+                                                             //
+                                                       // (1) DoSomething() 메서드 안에서
+                                                       // throw new Exception("arg가 10보다 큽니다."); 구문을 통해 
+                                                       // 예외를 던졌지만, DoSomething() 메서드 안에는
+                                                       // 이 예외를 처리할 수 있는 코드(예를 들어,try-catch 문이나
+                                                       // finally 블록, using 문)가 없습니다.
+                                                       // (2) 따라서 이 예외는 DoSomething() 메서드를 호출한
+                                                       // 호출자(여기서는 Main() 메서드)에게 던져집니다.
+                                                       // (3) 이렇게 던져진 예외는 메소드를 호출한 자(여기서는 Main() 메서드)의
+                                                       // try~catch 문에서 받아냅니다.
+        }
+
+
+        static void Main(string[] args) // Main 메서드: try-catch 문을 사용하여 예외 처리를 합니다.
+                                        // try 블록 안에는 DoSomething 메서드를 여러 번 호출하는 코드가 있습니다.
+                                        // DoSomething 메서드를 호출할 때, 
+                                        // arg 값이 10보다 크거나 같으면 Exception 예외가 발생합니다.
+        {
+            try
             {
-                for (int i = 0; i < 5; i++) // i가 0부터 4까지 반복함(for 루프를 5번 실행).
-                {
-                    Console.WriteLine(arr[i]); // arr[i]의 값을 출력합니다.
-                                               // i가 0, 1, 2일 때는
-                                               // 각각 arr[0], arr[1], arr[2]의 값인 1, 2, 3이 출력됩니다.
-                                               // i가 3, 4일 때는 arr[3], arr[4]에 접근하려고 시도하는데,
-                                               // 이는 배열의 범위를 벗어나는 인덱스입니다. 
-                                               // 따라서 IndexOutOfRangeException 예외가 발생합니다.
-                }
-            }
-            
-            
-            catch (IndexOutOfRangeException e) // 예외가 발생했을 때의 처리.
-                                               // try 절에서 코드를 처리하다가 예외가 던져지면
-                                               // catch 블록이 받아냅니다.
-                                               // 이때, catch 절은 try 블록에서 던질 예외 객체와 형식이 일치해야 합니다. 
-                                               // (예외 객체와 형식이 일치하지 않으면, 예외는 아무도 받지 못했기 때문에
-                                               // '처리하지 않은 예외'로 남게 됩니다.)
-                                               // try 블록에서 실행하는 코드에서 여러 종류의 예외를 던질 가능성이 있다면,
-                                               // 이를 받을 수 있는 catch 블록도 여러 개 둘 수 있습니다.
-                                               
-                                               // try 블록에서 IndexOutOfRangeException 예외가 발생하면,
-                                               // catch 블록이 실행됩니다.
-                                               // e는 예외 객체를 나타냅니다.
-            {
-                Console.WriteLine($"예외가 발생했습니다 : {e.Message}"); // 예외 메시지를 출력합니다.
+                DoSomething(1);
+                DoSomething(3);
+                DoSomething(5);
+                DoSomething(9);
+                DoSomething(11); // Exception 예외가 발생하고,
+                                 // catch 블록에서 예외 메시지 "arg가 10보다 큽니다."가 출력됩니다.
+
+                DoSomething(13); // DoSomething(11);에서 예외가 발생하여 이 코드는 실행되지 않습니다.
             }
 
-            Console.WriteLine("종료"); // "종료"를 출력합니다. 
-                                       //  try-catch 문을 사용하여 예외를 처리했기 때문에 
-                                       // 이 코드는 정상적으로 실행되고, 프로그램은 정상적으로 종료됩니다.
+            catch (Exception e) // Exception 타입의 예외를 처리합니다. 
+                                // Exception e는 발생한 예외 객체를 e 변수에 할당합니다. 
+            {
+                Console.WriteLine(e.Message); // 예외 객체의 메시지를 출력합니다.
+                                              // DoSomething(11)에서 Exception 예외가 발생하고, 
+                                              //  "arg가 10보다 큽니다."가 출력됩니다.
+            }
         }
     }
 }
@@ -60,9 +72,9 @@ namespace TryCatch
 /*
 출력 결과
 
-1
-2
-3
-예외가 발생했습니다 : Index was outside the bounds of the array.
-종료
+arg : 1
+arg : 3
+arg : 5
+arg : 9
+arg가 10보다 큽니다.
 */
