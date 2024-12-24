8f680a3c92942b397767328597e9e01972bb8c80
vvvvo0 <idvvvvo@gmail.com>
2024-12-24 20:41:01 +0900

    StackTrace 프로퍼티(예외 발생 위치 추적)

--- MainApp.cs
+++ MainApp.cs
@@ -2,63 +2,55 @@
 
 
 /*
-예외 필터(Exception Filter):
-catch 절이 받아들일 예외 객체에 제약 사항을 명시해서,
-해당 조건을 만족하는 예외 객체에 대해서만 예외 처리 코드를 실행할 수 있도록 함.
-catch() 절 뒤에 when 키워드를 이용해서 제약 조건을 기술하면 됨.
-when을 if라고 읽으면 이해하기 쉬움.
+예외 처리(Exception Handling) 장점:
+(1) 실제 일을 하는 코드(try 절)와 문제를 처리하는 코드(catch 절)를 분리시킴으로써,
+코드를 간결하게 만들어 줌.
 
-예외 필터링은 catch 블록에서 예외를 처리할지 여부를 결정하기 위해 조건을 추가하는 기능임.
-즉, 특정 조건을 만족하는 예외만 처리하고,
-그렇지 않은 예외는 다른 catch 블록이나 호출자에게 넘길 수 있음.
+(2) 예외 객체의 StackTrace 프로퍼티를 통해, 문제가 발생한 부분의 위치를 알려주기 때문에,
+디버깅이 용이함.
 
-장점?
-여러 catch 블록을 사용하지 않고도 다양한 조건에 따라 예외를 처리할 수 있음.
+(3) 여러 문제점을 하나로 묶거나, 코드에서 발생할 수 있는 오류를 종류별로 정리해줌.
+예를 들어, try 블록의 코드 중에서DivideByZeroException 예외를 일으킬 수 있는 부분이
+둘 이상일 수 있는데, 이 형식의 예외를 받는 catch 블록 하나면 모두 처리 가능함.
+
+StackTrace 프로퍼티의 활용?
+(1) 예외 발생 위치를 추적하여 디버깅을 용이하게 합니다.
+(2) 예외 발생 시점의 호출 스택 정보를 로깅하여 프로그램 분석에 활용할 수 있습니다.
+
+로깅?
+프로그램 실행 중에 발생하는 다양한 이벤트나 정보를 기록하는 과정.
+
+호출 스택 정보를 로깅한다는 것?
+예외 발생 시점의 호출 스택 정보를 로그에 기록하는 것을 의미.
+예외가 발생하면 프로그램은 현재 실행 중인 메서드, 그 메서드를 호출한 메서드, 
+또 그 메서드를 호출한 메서드 등을 순차적으로 따라 올라가면서 호출 스택 정보를 생성합니다.
+이 정보에는 각 메서드의 이름, 파일 이름, 줄 번호 등이 포함됩니다.
  */
 
 
-// 예외 필터링을 사용하는 방법을 보여줌.
-
-namespace ExceptionFiltering
+// 예외 처리(Exception Handling) 과정에서 StackTrace 프로퍼티를 사용하여
+// 예외 발생 위치를 추적하는 방법을 보여줌
+namespace StackTrace
 {
-    class FilterableException : Exception // Exception 클래스를 상속받아
-                                          // FilterableException이라는 사용자 정의 예외 클래스를 선언합니다.
-                                          // ErrorNo라는 정수형 프로퍼티를 정의하여 오류 번호를 저장합니다.
-    {
-        public int ErrorNo { get; set; }
-    }
-
-
     class MainApp
     {
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Number Between 0~10"); // 0에서 10 사이의 숫자를 입력하라는 메시지 출력
-            string input = Console.ReadLine(); // 사용자로부터 입력받은 문자열을 input 변수에 저장 
-
-            try // try 블록: 예외가 발생할 가능성이 있는 코드를 실행합니다.
+            try
             {
-                int num = Int32.Parse(input); // 입력받은 문자열(input)을 정수로 변환하여 num이라는 변수에 저장
-                                              // 만약 input이 숫자 형식이 아니면 FormatException 예외가 발생      
-
-                if (num < 0 || num > 10) // num이 0보다 작거나 10보다 크면,
-                    throw new FilterableException() { ErrorNo = num }; // FilterableException 예외를 발생시키고,
-                                                                       // ErrorNo 프로퍼티에 num 값을 저장
-                else // num이 0에서 10 사이의 값이면,
-                    Console.WriteLine($"Output : {num}"); // "Output : {num}"을 출력
+                int a = 1; // 정수형 변수 a를 선언하고 1로 초기화합니다.
+                Console.WriteLine(3 / --a); // a 값을 먼저 1 감소시킨 후(전위 감소 연산자),
+                                            // 3을 a로 나눈 결과를 출력하려고 시도합니다. 
+                                            // a 값이 0이 되므로,
+                                            // DivideByZeroException 예외가 발생합니다.
             }
 
-            catch (FilterableException e) when (e.ErrorNo < 0) // FilterableException 예외가 발생하고,
-                                                               // ErrorNo가 0보다 작으면,
+            catch (DivideByZeroException e) // DivideByZeroException 예외가 발생하면,
+                                            // catch 블록이 실행됩니다.
+                                            // e는 예외 객체를 나타내며,
+                                            // e.StackTrace는 예외 발생 위치의 호출 스택 정보를 문자열로 반환합니다.
             {
-                Console.WriteLine("Negative input is not allowed."); // "Negative input is not allowed." 메시지 출력
-            }
-
-            catch (FilterableException e) when (e.ErrorNo > 10) // FilterableException 예외 발생 시,
-                                                                // ErrorNo가 10보다 크면,
-            {
-                Console.WriteLine("Too big number is not allowed."); // "Too big number is not allowed." 메시지 출력
+                Console.WriteLine(e.StackTrace); // e.StackTrace 값을 콘솔에 출력합니다.
             }
         }
     }
@@ -66,24 +58,11 @@ namespace ExceptionFiltering
 
 
 /*
-출력 결과(15 입력)
+출력 결과
 
-Enter Number Between 0~10
-15
-Too big number is not allowed.
+   at StackTrace.MainApp.Main(String[] args) in C:\Users\M\source\repos\Hello\MainApp.cs:line 29
 */
-
-/*
-출력 결과(-5 입력)
-
-Enter Number Between 0~10
--5
-Negative input is not allowed.
-*/
-
-/*
-출력 결과(5 입력)
-
-Enter Number Between 0~10
-5
-Output : 5 */
+// 출력 결과는 예외가 발생한 위치를 나타냅니다.
+// 'StackTrace.MainApp.Main(String[] args)'는
+// MainApp 클래스의 Main 메서드에서 예외가 발생했음을 의미하고, 
+// 'C:\...\StackTrace\MainApp.cs:line 29' 는 예외가 발생한 파일과 줄 번호를 나타냅니다.
