696780b17d7fd83b557dbc90551b087c576f70f0
vvvvo0 <idvvvvo@gmail.com>
2024-12-26 15:47:54 +0900

    Statement Lambda(문 형식의 람다식)

--- MainApp.cs
+++ MainApp.cs
@@ -2,39 +2,40 @@
 
 
 /*
-람다식(Lambda Expression):
-익명 메서드를 만들기 위해 사용.
-람다식으로 만든 익명 메서드는 무명 함수(Anonymous Function)라고 부름.
-대리자보다 익명 메서드를 더욱 간결하게 작성할 수 있는 기능.
+Statement Lambda(문 형식의 람다식):
+여러 문장으로 구성된 코드 블록을 델리게이트에 할당할 수 있도록 합니다.
+반환 형식이 없는 무명 함수를 만들 수 있습니다.
+<-> 식 형식의 람다식: 반환 형식이 없는 무명 함수를 만들 수 없음.
  */
 
 
-namespace SimpleLambda
+// 문장을 입력하면 스페이스를 모두 지우는 프로그램
+namespace StatementLambda
 {
     class MainApp
     {
-        delegate int Calculate(int a, int b); // 익명 메서드를 만들려면 우선 대리자가 필요함.
-                                              // 따라서 Calculate라는 이름의 델리게이트를 선언합니다.
-                                              // 이 델리게이트는 두 개의 정수를 매개변수로 받아
-                                              // 정수 값을 반환하는 메서드를 참조할 수 있습니다.
+        delegate string Concatenate(string[] args); // Concatenate라는 이름의 델리게이트를 선언합니다.
+                                                    // 이 델리게이트는 문자열 배열을 매개변수로 받아 
+                                                    // 문자열 값을 반환하는 메서드를 참조할 수 있습니다.
 
         static void Main(string[] args)
         {
-            Calculate calc = (a, b) => a + b; // calc라는 Calculate 델리게이트 변수를 선언하고,
-                                              // 람다 식 (a, b) => a + b를 사용하여 초기화합니다.
-                                              // 람다 식은 (a, b)라는 매개변수 목록과
-                                              // a + b라는 식으로 구성됩니다. 
-                                              // 이 람다 식은 두 개의 정수를 더하는 기능을 수행합니다.
 
-            Console.WriteLine($"{3} + {4} : {calc(3, 4)}"); // calc(3,4): calc 델리게이트를 호출하여,
-                                                            // 3과 4를 더한 결과 반환합니다.
+            // Concatenate concat = (arr) => { ... };:
+            // concat라는 Concatenate 델리게이트 변수를 선언하고, 문 형식({})의 람다 식을 사용하여 초기화합니다. 
+            // 람다 식은 arr 라는 매개변수 목록과 {} 안에 여러 문장으로 구성된 코드 블록으로 구성됩니다.
+            Concatenate concat = (arr) =>
+                {
+                    string result = "";
+                    foreach (string s in arr)
+                        result += s;
+
+                    return result;
+                };
+
+               Console.WriteLine(concat(args)); // concat 델리게이트를 호출하여,
+                                                // args 배열을 연결한 결과를 출력합니다.
+            
         }
     }
 }
-
-
-/*
-출력 결과
-
-3 + 4 : 7
-*/
