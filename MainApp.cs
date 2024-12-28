0478be183d1a0a17aa7bf4bc78324888f86764c8
vvvvo0 <idvvvvo@gmail.com>
2024-12-28 23:51:27 +0900

    LINQ: where, orderby, select

--- MainApp.cs
+++ MainApp.cs
@@ -1,58 +1,81 @@
 ﻿using System;
-using System.Linq;
+using System.Collections.Generic; // List<T>와 같은 제네릭 컬렉션을 사용하기 위해 필요
+                                  // List<T>는 배열과 유사하지만 크기가 동적으로 변하는 컬렉션
+using System.Linq; // LINQ(Language Integrated Query)를 사용하기 위해 필요 
 
 
 /*
-LINQ:
-Language Integrated Query의 약자로, C#에 톱합된 데이터 질의 기능임.
-데이터 질의(Query)란, 데이터에 대해 물어본다는 의미.
-데이터를 가공하기 전에 필요한 데이터를 찾아내는 일을 도와줌.
-컬렉션을 편리하게 다루기 위한 목적으로 만들어진 질의(Query) 언어.
+where:
+필터(Filter) 역할.
+from 절이 데이터 원본으로부터 뽑아낸 범위 변수가 가져야 하는 조건을 
+where 연산자에 인수로 입력하면, LINQ는 해당 조건에 부합하는 데이터만 걸러냅니다.
 
-from:
-모든 LINQ 쿼리식(Query Expression)은 반드시 form 절로 시작합니다.
-쿼리식의 대상이 될 데이터 원본(Data Source)과 
-데이터 원본 안에 들어 있는 각 요소 데이터를 나타내는 범위 변수(Range Variable)를 
-from 절에서 지정해줘야 합니다.
-from 절을 이용해서 데이터 원본으로부터 범위 변수를 뽑아낸 후에, 
-LINQ가 제공하는 연산자(where, orderby, select 등)를 이용해서 데이터를 가공 및 추출할 수 있습니다.
+orderby:
+데이터의 정렬을 수행하는 연산자입니다.
 
-from 절의 '데이터 원본':
-IEnumerable<T> 인터페이스를 상속하는 형식이어야만 합니다.
-배열이나 컬렉션 객체들은 IEnumerable<T> 인터페이스를 상속하기 때문에
-from 절의 데이터 원본으로 사용할 수 있습니다.
+select:
+select 절은 최종 결과를 추출합니다.
 
-from 절의 '범위 변수':
-쿼리 변수(Query Variable)라고도 하는데, foreach 문의 반복 변수를 생각하면 이해하기 쉬움.
-foreach(int x in arr)에서 x.
-
-LINQ의 '범위 변수' vs foreach 문의 '반복 변수'?
-foreach 문의 반복 변수는 데이터 원본으로부터 데이터를 담아내는 반면,
-범위 변수는 실제로 데이터를 담지는 않음.
-그래서 쿼리식 외부에서 선언된 변수에, 범위 변수의 데이터를 복사해 넣는 등의 일은 할 수 없음.
-범위 변수는 오로지 LINQ 질의 안에서만 통용되며, 
-질의가 실행될 떄 어떤 일이 일어날지를 묘사하기 위해 도입되었기 때문.
-*/
+->
+from 절에서 데이터 원본으로부터 범위 변수를 뽑아내고,
+where 절에서 이 범위 변수의 조건을 검사한 후,
+orderby 절에서 그 결과를 정렬하고,
+select 문을 이요하여 최종 결과를 추출합니다.
+ */
 
 
-// LINQ를 사용하여 정수 배열에서 짝수를 찾아 오름차순으로 정렬
-namespace From
+// List<Profile>을 사용하여 Profile 객체의 컬렉션을 생성하고,
+// LINQ를 사용하여 컬렉션을 쿼리하고 결과를 출력합니다.
+namespace SimpleLinq
 {
+    class Profile // Name과 Height 프로퍼티를 가진 클래스 Profile
+    {
+        public string Name { get; set; }
+        public int Height { get; set; }
+    }
+
     class MainApp
     {
         static void Main(string[] args)
         {
-            int[] numbers = { 9, 2, 6, 4, 5, 3, 7, 8, 1, 10 }; 
-            // 10개의 정수 값을 가진 numbers 배열을 선언하고 초기화합니다.
+            Profile[] arrProfile = // 5개의 Profile 객체를 생성하여
+                                   // arrProfile 배열에 저장합니다.
+            {
+                new Profile(){Name="정우성", Height=186},
+                new Profile(){Name="김태희", Height=158},
+                new Profile(){Name="고현정", Height=172},
+                new Profile(){Name="이문세", Height=178},
+                new Profile(){Name="하하", Height=171}
+            };
 
-            var result = from n in numbers // numbers 배열(데이터 원본) 안에 있는 각 요소를
-                                           // n이라는 변수(범위 변수)에 할당하며 쿼리를 시작합니다.
-                         where n % 2 == 0 // n을 2로 나눈 나머지가 0인 경우(즉, 짝수인 경우)에만, 요소를 선택합니다.
-                         orderby n // 선택된 요소를 n 값을 기준으로 오름차순으로 정렬합니다.
-                         select n; // 정렬된 요소를 선택하여 result 변수에 저장합니다.
 
-            foreach (int n in result) // result에 저장된 요소를 순회하며 각 요소를 n이라는 변수에 할당합니다.
-                Console.WriteLine($"짝수 : {n}"); // n 값을 "짝수 : {n}" 형식으로 출력합니다.
+            // var 형식으로 선언된 profiles의 실제 형식?
+            // LINQ 질의(쿼리) 결과는 IEnumerable<T>로 반환되는데, 이때 형식 매개변수 T는 select 문에 의해 결정됨.
+            // select new { Name = profile.Name, InchHeight = profile.Height * 0.393 } 부분에서
+            // new { ... } 구문을 사용하여 익명 형식을 생성하고 있음.
+            // 따라서 profiles 변수는 익명 형식임.
+            // 익명 형식은 이름이 없는 클래스와 같습니다. 컴파일러가 익명 형식의 이름을 자동으로 생성하고,
+            // Name과 InchHeight 프로퍼티를 가진 클래스를 정의합니다.
+            // var 키워드는 컴파일러가 변수의 타입을 자동으로 유추하도록 하는 키워드입니다.
+            // 이 경우 컴파일러는 select 문의 결과를 보고 profiles 변수의 타입을 익명 형식으로 유추합니다.
+            // 따라서 profiles 변수는 Name과 InchHeight 프로퍼티를 가진 익명 형식의 객체를 저장하는 컬렉션입니다.
+            var profiles = from profile in arrProfile // arrProfile 배열의 각 요소를
+                                                      // profile이라는 변수에 할당
+
+                           where profile.Height < 175 // profile의 Height 프로퍼티 값이 175보다 작은 요소만 선택하고,
+
+                           orderby profile.Height // 선택된 요소를 Height 프로퍼티 값을 기준으로 오름차순으로 정렬
+
+                           select new // new { ... } 구문(익명 형식)을 사용하여,
+                                      // Name과 InchHeight 프로퍼티를 가진 새로운 객체를 생성합니다.
+                                      // InchHeight는 Height 값에 0.393을 곱하여 센티미터를 인치로 변환한 값입니다.
+                           {
+                               Name = profile.Name,
+                               InchHeight = profile.Height * 0.393
+                           };
+
+            foreach (var profile in profiles) // profiles 쿼리 결과를 순회하며 각 요소를 profile이라는 변수에 할당합니다.
+                Console.WriteLine($"{profile.Name}, {profile.InchHeight}"); // profile의 Name과 InchHeight 프로퍼티 값을 출력합니다.
         }
     }
 }
@@ -61,9 +84,7 @@ namespace From
 /*
 출력 결과
 
-짝수 : 2
-짝수 : 4
-짝수 : 6
-짝수 : 8
-짝수 : 10
+김태희, 62.204
+하하, 67.322
+고현정, 67.716
 */
