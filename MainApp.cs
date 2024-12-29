01c29748f4b19ef1b17b4c271c7eaa2b28649b18
vvvvo0 <idvvvvo@gmail.com>
2024-12-29 17:04:43 +0900

    LINQ: from 문 중첩

--- MainApp.cs
+++ MainApp.cs
@@ -1,81 +1,47 @@
 ﻿using System;
-using System.Collections.Generic; // List<T>와 같은 제네릭 컬렉션을 사용하기 위해 필요
-                                  // List<T>는 배열과 유사하지만 크기가 동적으로 변하는 컬렉션
-using System.Linq; // LINQ(Language Integrated Query)를 사용하기 위해 필요 
+using System.Linq;
 
 
 /*
-where:
-필터(Filter) 역할.
-from 절이 데이터 원본으로부터 뽑아낸 범위 변수가 가져야 하는 조건을 
-where 연산자에 인수로 입력하면, LINQ는 해당 조건에 부합하는 데이터만 걸러냅니다.
-
-orderby:
-데이터의 정렬을 수행하는 연산자입니다.
-
-select:
-select 절은 최종 결과를 추출합니다.
-
-->
-from 절에서 데이터 원본으로부터 범위 변수를 뽑아내고,
-where 절에서 이 범위 변수의 조건을 검사한 후,
-orderby 절에서 그 결과를 정렬하고,
-select 문을 이요하여 최종 결과를 추출합니다.
+from 문 중첩:
+여러 개의 데이터 원본에 질의(쿼리)하기
  */
 
 
-// List<Profile>을 사용하여 Profile 객체의 컬렉션을 생성하고,
-// LINQ를 사용하여 컬렉션을 쿼리하고 결과를 출력합니다.
-namespace SimpleLinq
+// from 문을 중첩하면 여러 개의 데이터 원본에서 데이터를 쿼리할 수 있음을 보여줌
+namespace FromFrom
 {
-    class Profile // Name과 Height 프로퍼티를 가진 클래스 Profile
+    class Class // Name과 Score 프로퍼티를 가집니다.
+                // Name은 문자열 타입이고, Score는 정수형 배열 타입입니다.
     {
         public string Name { get; set; }
-        public int Height { get; set; }
+        public int[] Score { get; set; }
     }
 
     class MainApp
     {
         static void Main(string[] args)
         {
-            Profile[] arrProfile = // 5개의 Profile 객체를 생성하여
-                                   // arrProfile 배열에 저장합니다.
+            Class[] arrClass =
             {
-                new Profile(){Name="정우성", Height=186},
-                new Profile(){Name="김태희", Height=158},
-                new Profile(){Name="고현정", Height=172},
-                new Profile(){Name="이문세", Height=178},
-                new Profile(){Name="하하", Height=171}
+                new Class(){Name="연두반", Score=new int[]{99, 80, 70, 24}},
+                new Class(){Name="분홍반", Score=new int[]{60, 45, 87, 72}},
+                new Class(){Name="파랑반", Score=new int[]{92, 30, 85, 94}},
+                new Class(){Name="노랑반", Score=new int[]{90, 88, 0, 17}}
             };
 
+            var classes = from c in arrClass // arrClass 객체에 from 절로 접근해서 범위 변수 c를 뽑고,
+                          from s in c.Score // 다시 그 c 객체의 Score 필드에 또 다른 from 절로 접근해서
+                                            // 새로운 범위 변수 s를 뽑습니다.
+                                            // 범위 변수 s는 개별 점수를 나타냅니다.
+                          where s < 60 // where 절을 사용해서 이 s가 60보다 낮은지 걸러내고,
+                          orderby s
+                          select new { c.Name, Lowest = s }; // new {...} 부분을 통해 무명 형식을 선언해서
+                                                             // 낙제점을 맞은 학생의 학급 이름과 점수를 담아냅니다.
 
-            // var 형식으로 선언된 profiles의 실제 형식?
-            // LINQ 질의(쿼리) 결과는 IEnumerable<T>로 반환되는데, 이때 형식 매개변수 T는 select 문에 의해 결정됨.
-            // select new { Name = profile.Name, InchHeight = profile.Height * 0.393 } 부분에서
-            // new { ... } 구문을 사용하여 익명 형식을 생성하고 있음.
-            // 따라서 profiles 변수는 익명 형식임.
-            // 익명 형식은 이름이 없는 클래스와 같습니다. 컴파일러가 익명 형식의 이름을 자동으로 생성하고,
-            // Name과 InchHeight 프로퍼티를 가진 클래스를 정의합니다.
-            // var 키워드는 컴파일러가 변수의 타입을 자동으로 유추하도록 하는 키워드입니다.
-            // 이 경우 컴파일러는 select 문의 결과를 보고 profiles 변수의 타입을 익명 형식으로 유추합니다.
-            // 따라서 profiles 변수는 Name과 InchHeight 프로퍼티를 가진 익명 형식의 객체를 저장하는 컬렉션입니다.
-            var profiles = from profile in arrProfile // arrProfile 배열의 각 요소를
-                                                      // profile이라는 변수에 할당
-
-                           where profile.Height < 175 // profile의 Height 프로퍼티 값이 175보다 작은 요소만 선택하고,
-
-                           orderby profile.Height // 선택된 요소를 Height 프로퍼티 값을 기준으로 오름차순으로 정렬
-
-                           select new // new { ... } 구문(익명 형식)을 사용하여,
-                                      // Name과 InchHeight 프로퍼티를 가진 새로운 객체를 생성합니다.
-                                      // InchHeight는 Height 값에 0.393을 곱하여 센티미터를 인치로 변환한 값입니다.
-                           {
-                               Name = profile.Name,
-                               InchHeight = profile.Height * 0.393
-                           };
-
-            foreach (var profile in profiles) // profiles 쿼리 결과를 순회하며 각 요소를 profile이라는 변수에 할당합니다.
-                Console.WriteLine($"{profile.Name}, {profile.InchHeight}"); // profile의 Name과 InchHeight 프로퍼티 값을 출력합니다.
+            foreach (var c in classes) // classes 쿼리 결과를 순회하면서 각 익명 객체를 c 변수에 할당합니다.
+                Console.WriteLine($"낙제 : {c.Name} ({c.Lowest})"); // c 변수를 통해 익명 객체의 Name과 Lowest 프로퍼티에 접근하여
+                                                                    // "낙제 : {반 이름} ({낙제 점수})" 형식으로 출력합니다.
         }
     }
 }
@@ -84,7 +50,9 @@ namespace SimpleLinq
 /*
 출력 결과
 
-김태희, 62.204
-하하, 67.322
-고현정, 67.716
+낙제 : 노랑반 (0)
+낙제 : 노랑반 (17)
+낙제 : 연두반 (24)
+낙제 : 파랑반 (30)
+낙제 : 분홍반 (45)
 */
