6f9db2c6a47e6bad0a008d159bbb6b296ac28003
vvvvo0 <idvvvvo@gmail.com>
2025-01-04 15:19:57 +0900

    WinForm: Form 위에 컨트롤 올리기

--- MainApp.cs
+++ MainApp.cs
@@ -3,50 +3,67 @@ using System.Windows.Forms; // 윈도우 폼 애플리케이션을 만들기 위
 
 
 /*
-MaximizeBox, MinimizeBox 프로퍼티:
-boolean 형식,
-버튼을 창에 표시하고자 할 때는 true를 입력하고,
-버튼을 창에서 감추고자 할 때는 false를 입력합니다.
+컨트롤(Control):
+윈도우 OS가 제공하는 UI 요소를 말합니다.
+응용 프로그램을 제어(컨트롤)하는 데 사용하는 도구라는 의미에서 붙은 이름입니다.
+(버튼, 텍스트 박스 등)
 
+윈도우 OS는 UI를 위해 버튼, 텍스트박스, 메뉴, 콤보박스, 리스트뷰 등과 같은 '표준 컨트롤'을 제공합니다.
+.NET의 WinForm은 이 '표준 컨트롤'을 간편하게 창 위에 올리 수 있도록 잘 포장해놨습니다.
+이 컨트롤들을 제어하는 데 필요한 메서드와 프로퍼티, 이벤트 또한 잘 정리해 놨습니다.
 
-Text 프로퍼티:
-창의 제목을 나타내는 프로퍼티로,
-string 형식입니다.
-표시하려는 제목을 문자열로 입력해주면 창의 제목이 변경됩니다.
+GUI 플랫폼(자바의 스윙이나 유닉스의 모티브)에서는 컨트롤을 위젯(Widget: Window Gadget)이라고 부르고,
+델파이에서는 컨트롤을 VCL(Visual Component Library)이라고 부릅니다.
+
+UI(User Interface):
+응용 프로그램과 사용자가 대화를 하는 창구입니다.
+사용자와 컴퓨터 시스템 간의 상호 작용을 위한 접점을 의미합니다. 
+쉽게 말해, 사용자가 컴퓨터나 프로그램을 사용할 때 보고 만지는 모든 것들이 UI에 속합니다.
+웹사이트의 버튼, 메뉴, 입력 필드, 윈도우 프로그램의 창, 아이콘, 메뉴바 등이 모두 UI입니다.
+스마트폰 앱의 터치스크린, 버튼, 화면 구성 요소들도 UI에 해당합니다.
  */
 
 
-// 윈도우 폼 애플리케이션 창의 최소화/최대화 버튼을 마우스 클릭으로 활성화/비활성화
-namespace FormStyle
+// System.Windows.Forms 네임스페이스를 사용하여, 버튼 컨트롤을 가진 윈도우 폼 애플리케이션을 만듦.
+// (버튼을 가진 윈도우 창이 나타나고, 버튼을 사용자가 클릭할 때마다 '딸깍!'이라는 메시지를 출력하는
+// 메시지 박스가 나타나도록 만들었습니다.)
+namespace FormAndControl
 {
     class MainApp : Form // Form 클래스를 상속받는 MainApp 클래스 정의
     {
         static void Main(string[] args)
         {
-            MainApp form = new MainApp(); // MainApp의 새 인스턴스를 생성합니다.
+            // (1) 컨트롤의 인스턴스 생성(System.Windows.Forms.Button 클래스의 인스턴스를 생성)
+            Button button = new Button(); // Button 컨트롤의 새 인스턴스를 생성합니다.
 
-            form.Width = 400; // 윈도우 창의 너비를 400으로 설정합니다.
-            form.MouseDown += new MouseEventHandler(form_MouseDown); // 윈도우 창의 MouseDown 이벤트에 form_MouseDown 메서드를 연결합니다.
+
+            // (2) 컨트롤의 프로퍼티에 값을 지정
+            button.Text = "Click Me!"; // 버튼의 텍스트를 "Click Me!"로 설정합니다.
+            button.Left = 100; // 버튼의 왼쪽 위치를 100으로 설정합니다.
+            button.Top = 50; // 버튼의 위쪽 위치를 50으로 설정합니다.
+
+
+            // (3) 컨트롤의 이벤트에 이벤트 처리기 등록
+            // 버튼의 Click 이벤트에 익명 메서드를 추가합니다.
+            // 익명 메서드는 버튼이 클릭될 때 "딸깍!" 메시지 박스를 표시합니다.
+            // (이벤트 처리기를 따로 메서드로 선언하지 않고 람다식으로 구현해봄)
+            button.Click += (object sender, EventArgs e) => { MessageBox.Show("딸깍!"); };
+
+
+            // (4) 폼에 컨트롤 추가
+            MainApp form = new MainApp(); // MainApp 윈도우 창의 새 인스턴스를 생성합니다.
+                                          // (form의 인스턴스를 생성)
+            form.Controls.Add(button); // 윈도우 창에 button 컨트롤을 추가합니다.
+                                       // (form 인스턴스에서 Controls 프로퍼티의 add() 메서드를 호출하여,
+                                       //  선언한 button 객체를 Form에 올립니다.)
+
+            form.Text = "Form & Control"; // 윈도우 창의 제목을 "Form & Control"로 설정합니다.
+            form.Height = 150; // 윈도우 창의 높이를 150으로 설정합니다.
+
+
+
 
             Application.Run(form); // 윈도우 창을 표시하고 메시지 루프를 시작합니다.
-        }
-
-        static void form_MouseDown(object sender, MouseEventArgs e) // 마우스 버튼을 누를 때 발생하는 이벤트를 처리하는 메서드
-        {
-            Form form = (Form)sender; // 이벤트를 발생시킨 객체를 Form 타입으로 변환합니다.
-
-            if (e.Button == MouseButtons.Left) // 마우스 왼쪽 버튼을 누르면
-            {
-                form.MaximizeBox = true; // 최대화 버튼을 활성화합니다.
-                form.MinimizeBox = true; // 최소화 버튼을 활성화합니다.
-                form.Text = "최소화/최대화 버튼이 활성화되었습니다."; // 폼의 제목을 변경합니다.
-            }
-            else if (e.Button == MouseButtons.Right) // 마우스 오른쪽 버튼을 누르면
-            {
-                form.MaximizeBox = false; // 최대화 버튼을 비활성화합니다.
-                form.MinimizeBox = false; // 최소화 버튼을 비활성화합니다.
-                form.Text = "최소화/최대화 버튼이 비활성화되었습니다."; // 폼의 제목을 변경합니다.
-            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 SimpleWindow:
 ! [1](https://github.com/user-attachments/assets/4c7d39e7-e66d-45ef-9de2-63cc2cf59429)
+
+WinForm: Form 위에 컨트롤 올리기
+![스크린샷 2025-01-04 144954](https://github.com/user-attachments/assets/54302383-39c3-418c-bdb6-0189e67f9789)
+![스크린샷 2025-01-04 145016](https://github.com/user-attachments/assets/227f5052-03b2-4801-8283-6821f75fa9ec)
