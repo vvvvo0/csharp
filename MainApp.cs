b88ab497a7894331cc2750e625c011b3e36a7079
vvvvo0 <idvvvvo@gmail.com>
2025-01-08 13:12:22 +0900

    스레드 상태 변화(ThreadState 열거형에 정의해둠)

--- MainApp.cs
+++ MainApp.cs
@@ -1,41 +1,45 @@
 ﻿using System;
 using System.Threading; // 스레드 사용을 위한 네임스페이스
 
-namespace BasicThread
+namespace UsingThreadState
 {
     class MainApp
     {
-        static void DoSomething() // 스레드가 실행할 메서드
+        private static void PrintThreadState(ThreadState state) // ThreadState 값과 해당하는 정수 값을 출력하는 메서드
         {
-            for (int i = 0; i < 5; i++) // 0부터 4까지 5번 반복
-            {
-                Console.WriteLine($"DoSomething : {i}"); // "DoSomething : {i}" 출력
-                Thread.Sleep(10); // 10밀리초 동안 스레드 실행 중지
-            }
+            Console.WriteLine("{0,-16} : {1}", state, (int)state);
         }
 
         static void Main(string[] args)
         {
-            // (1) Thread의 인스턴스 생성
-            Thread t1 = new Thread(new ThreadStart(DoSomething)); // DoSomething 메서드를 실행할 스레드 t1 생성
-
-            Console.WriteLine("Starting thread..."); // "Starting thread..." 출력
-            
-            // (2) 스레드 시작
-            t1.Start(); // 스레드 t1 시작
-
-            for (int i = 0; i < 5; i++) // 0부터 4까지 5번 반복
-            {
-                Console.WriteLine($"Main : {i}"); // "Main : {i}" 출력
-                Thread.Sleep(10); // 10밀리초 동안 메인 스레드 실행 중지
-            }
-
-            Console.WriteLine("Wating until thread stops..."); // "Wating until thread stops..." 출력
-            
-            // (3) 스레드의 종료 대기
-            t1.Join(); // 스레드 t1이 종료될 때까지 기다림
-
-            Console.WriteLine("Finished"); // "Finished" 출력
+            PrintThreadState(ThreadState.Running); // Running 상태 출력
+            PrintThreadState(ThreadState.StopRequested); // StopRequested 상태 출력
+            PrintThreadState(ThreadState.SuspendRequested); // SuspendRequested 상태 출력
+            PrintThreadState(ThreadState.Background); // Background 상태 출력
+            PrintThreadState(ThreadState.Unstarted); // Unstarted 상태 출력
+            PrintThreadState(ThreadState.Stopped); // Stopped 상태 출력
+            PrintThreadState(ThreadState.WaitSleepJoin); // WaitSleepJoin 상태 출력
+            PrintThreadState(ThreadState.Suspended); // Suspended 상태 출력
+            PrintThreadState(ThreadState.AbortRequested); // AbortRequested 상태 출력
+            PrintThreadState(ThreadState.Aborted); // Aborted 상태 출력
+            PrintThreadState(ThreadState.Aborted | ThreadState.Stopped); // Aborted | Stopped 상태 출력
         }
     }
 }
+
+/*
+출력 결과
+
+Running          : 0
+StopRequested    : 1
+SuspendRequested : 2
+Background       : 4
+Unstarted        : 8
+Stopped          : 16
+WaitSleepJoin    : 32
+Suspended        : 64
+AbortRequested   : 128
+Aborted          : 256
+Stopped, Aborted : 272
+ */
+// ThreadState의 요소값이 2의 제곱으로 증가하는 모습을 보임.
